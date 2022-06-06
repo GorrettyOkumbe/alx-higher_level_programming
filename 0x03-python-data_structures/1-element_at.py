e894d0e3c5720fe7dab8c159cc64528c0b616ea5
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-06-06 03:37:25 -0700

    indexing

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    for i in range(len(my_list)):
+            if idx > (len(my_list) - 1) or idx < 0:
+                return None
+            else:
+                return my_list[idx]
