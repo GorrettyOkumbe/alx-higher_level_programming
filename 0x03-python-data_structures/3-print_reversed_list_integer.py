7075aa7252ad68bfd0369ad0d9a73662a7029f52
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-06-06 04:25:03 -0700

    check if list then reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in my_list[::-1]:
-        if isinstance(my_list, list):
+    if isinstance(my_list, list):  # first check if it is a list
+        for i in my_list[::-1]:  # then iterate through and reverse the list
             print("{:d}".format(i))
