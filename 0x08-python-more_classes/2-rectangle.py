24633a36f12ef639cba639fa112d2f84e41f705b
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-06-27 15:28:18 -0700

    area and perimeter of rect

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,6 +57,5 @@ class Rectangle:
 
         if self.width == 0 or self.height == 0:
             return 0
-        
-        return 2 * (self.width + self.height)
 
+        return 2 * (self.width + self.height)
