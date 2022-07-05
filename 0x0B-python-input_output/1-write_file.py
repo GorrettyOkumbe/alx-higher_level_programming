249309567fc06e7832da6d4db2d76bc37a5d9355
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-05 22:56:43 +0300

    writing to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""1-write module"""
+
+
+def write_file(filename="", text=""):
+    """writes to a file"""
+    with open(filename, "w", encoding="utf-8")  as file:
+        write_data = file.write(text)
+        return write_data
