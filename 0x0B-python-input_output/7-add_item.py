509ad7a42a586cfd9db998de4d555c35dcb84347
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-06 06:31:28 +0300

    adding items to CLi

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,17 +2,17 @@
 """7-add_item module"""
 import sys
 
+if __name__ == "__main__":
+    saving = __import__('5-save_to_json_file').save_to_json_file
+    loading = __import__('6-load_from_json_file').load_from_json_file
 
-saving = __import__('5-save_to_json_file').save_to_json_file
-loading = __import__('6-load_from_json_file').load_from_json_file
-
-file = "add_item.json"
-file_save = []
-
-try:
-    loading(file)
-except FileNotFoundError:
+    file = "add_item.json"
     file_save = []
 
-file_save.extend(sys.argv[1:])
-saving(file_save, file)
+    try:
+        loading(file)
+    except FileNotFoundError:
+        file_save = []
+
+    file_save.extend(sys.argv[1:])
+    saving(file_save, file)
