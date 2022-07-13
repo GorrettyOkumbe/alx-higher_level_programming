04ec2917a2b128b7798fd22ab47bd4456921898f
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-14 01:00:11 +0300

    child class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """square constructor"""
 
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """string rep of a square"""
