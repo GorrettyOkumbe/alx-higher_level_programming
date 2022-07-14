97ce7c6560d90c58a1f30f797218bcf9937c8810
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-15 00:25:22 +0300

    static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns json string representation"""
-        if list_dictionaries is None or  not list_dictionaries:
+        if list_dictionaries is None or not list_dictionaries:
             return "[]"
         else:
-            return  json.dumps(list_dictionaries)
+            return json.dumps(list_dictionaries)
