18e86f818ad2aa901933d5828a4908ee97321938
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-14 18:24:21 +0300

    dictionary rep of Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -44,3 +44,7 @@ class Square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """returns dictionary representation of the Square class"""
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
