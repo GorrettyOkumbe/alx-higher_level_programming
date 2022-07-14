43a845a9ac588d616331c7bd89e1cc9808a376f7
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-07-14 18:16:32 +0300

    dictionary rep of rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -107,3 +107,8 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        """returns dictionary representation of this class(Rectangle)"""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
