0a68aebb8fcd7a2b0d754a2b04151b922b44653a
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-08-10 02:53:38 +0300

    use of alias

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- Script that computes the score average of all records in the table second_table
 -- The result column name should be average
 
-SELECT AVG(score) AS avarage FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
