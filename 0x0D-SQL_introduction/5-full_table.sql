b3b6c05c5c0bcbe30083ede90286f1b61d0eae1f
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-08-10 01:11:43 +0300

    show full description of a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Script that prints the full description of the table first_table from the database hbtn_0c_0
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
 
-SHOW CREATE first_tabl;
+SHOW CREATE first_table;
