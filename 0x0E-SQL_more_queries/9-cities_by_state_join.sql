8b97a75eb18600c7c8b9901a026e1e5d16f7f1d4
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-08-11 16:28:03 +0300

    cities by stses

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
 
-SELECT c.`id``, c.`name``, s.`name`
-	FROM `cities` AS c
-		INNER JOIN `states` AS s ON c.`state_id` = s.`id`;
-ORDER BY c.`id`;
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id=states.id
+ORDER BY cities.id;
