8b66b8e7445251ac2937eaff62d4fe6bc7044b62
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-09-06 12:52:21 +0300

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const y = process.argv[2];
-if(isNaN(y)){
+if (isNaN(y)){
 	console.log("Missing size");
 }else{
-	for(let i = 0; i < y; i++){
+	for (let i = 0; i < y; i++){
 		console.log('X'.repeat(y));
 }
 }
