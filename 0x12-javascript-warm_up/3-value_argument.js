7a6cac6bfa540777a1e43bcfd474b5a5d9137a44
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-09-06 10:09:58 +0300

    value_arguement

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+let count = 0;
+let args = process.argv
+
+for(let i in args){
+	count += 1
+}
+if (count === 2){
+	console.log("No argument");
+}
+else if (count > 2){
+	console.log(args[2]);
+}
