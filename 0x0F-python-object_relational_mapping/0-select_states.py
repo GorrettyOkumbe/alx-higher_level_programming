185dc78177fc964ef81145dc8739e1d155d017b0
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-09-15 17:43:37 +0300

    selecting from states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,13 +2,15 @@
 """0-select_states module"""
 import sys
 import MySQLdb
-args = sys.argv
-conn = MySQLdb.connect(host="localhost", port=3306,
-                       user=args[1], passwd=args[2], db=args[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM `states` ORDER BY states.id ASC ")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-conn.close()
+
+if __name__ == "__main__":
+    args = sys.argv
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=args[1], passwd=args[2], db=args[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY states.id ASC ")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
