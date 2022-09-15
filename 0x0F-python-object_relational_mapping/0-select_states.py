8d2e9fe1c4ed32982fa961ab9f04eafad365c856
GorrettyOkumbe <okumbegorretty4@gmail.com>
2022-09-15 17:27:33 +0300

    selecting from states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0-select_states module"""
+import sys
+import MySQLdb
+args = sys.argv
+conn = MySQLdb.connect(host="localhost", port=3306, user=args[1], passwd=args[2], db=args[3], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM `states` ORDER BY states.id ASC ")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+conn.close()
